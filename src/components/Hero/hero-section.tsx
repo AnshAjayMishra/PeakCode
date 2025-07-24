dfa2e4b621e1f0b739a011b0f85f68ec4b45bbb3
AnshAjayMishra <ajayanshmishra10@gmail.com>
2025-07-25 00:33:11 +0530

    commit

--- src/components/Hero/hero-section.tsx
+++ src/components/Hero/hero-section.tsx
@@ -42,13 +42,13 @@ export function HeroSection() {
         <Navbar/>
            
             <main className="overflow-hidden">
-                <div
+                {/* <div
                     aria-hidden
                     className="z-[2] absolute inset-0 pointer-events-none isolate opacity-50 contain-strict hidden lg:block">
                     <div className="w-[35rem] h-[80rem] -translate-y-[350px] absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
                     <div className="h-[80rem] absolute left-0 top-0 w-56 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
                     <div className="h-[80rem] -translate-y-[350px] absolute left-0 top-0 w-56 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
-                </div>
+                </div> */}
                 <section>
                     <div className="relative pt-24 md:pt-36">
                         <AnimatedGroup
@@ -77,13 +77,13 @@ export function HeroSection() {
                                 },
                             }}
                             className="absolute inset-0 -z-20">
-                            <Image
+                            {/* <Image
                                 src="https://ik.imagekit.io/lrigu76hy/tailark/night-background.jpg?updatedAt=1745733451120"
                                 alt="background"
                                 className="absolute inset-x-0 top-56 -z-20 hidden lg:top-32 dark:block"
                                 width="3276"
                                 height="4095"
-                            />
+                            /> */}
                         </AnimatedGroup>
                         <div aria-hidden className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--background)_75%)]" />
                         <div className="mx-auto max-w-7xl px-6">
@@ -108,11 +108,11 @@ export function HeroSection() {
                                     </Link>
                         
                                     <h1
-                                        className="mt-8 max-w-4xl mx-auto text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem]">
+                                        className="mt-8 max-w-4xl mx-auto text-balance text-4xl md:text-5xl lg:mt-16 xl:text-[4.70rem]">
                                         Crack DSA & System Design Like a Pro
                                     </h1>
                                     <p
-                                        className="mx-auto mt-8 max-w-2xl text-balance text-lg">
+                                        className="mx-auto mt-8 max-w-2xl text-balance text-md">
                                         Level-wise problems. Real-world system design. All in one place.
                                         Built for developers aiming to crack top tech interviews with confidence.
                                     </p>
